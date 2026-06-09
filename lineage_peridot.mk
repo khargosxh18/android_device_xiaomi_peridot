5346f0503f23cdb38e0df35d18c34618994de88b
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-06-09 11:26:13 +0000

    peridot: Welcome to ProjectSakura!

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Aicp stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from peridot device
 $(call inherit-product, device/xiaomi/peridot/device.mk)
 
-PRODUCT_NAME := aicp_peridot
+PRODUCT_NAME := lineage_peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -31,6 +31,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# AICP Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="Khargosxh18"
+# Some Sakura Goodies
+SAKURA_MAINTAINER := Khargosxh18
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+SAKURA_BUILD_TYPE := gapps
+
+TARGET_HAS_UDFPS:= true
