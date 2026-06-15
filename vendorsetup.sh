ffbb81b73a55b3cab0123060594caa8b796af845
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-06-15 10:19:36 +0000

    peridot: integrate automatic fetch for GameBar, BCR and ViPER4Android

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,15 @@
 rm -rf vendor/sakura-priv
 
 git clone https://github.com/Sakura-Devices/vendor_sakura-priv vendor/sakura-priv
+
+# Gamebar
+rm -rf packages/apps/GameBar
+git clone https://github.com/kenway214/packages_apps_GameBar.git packages/apps/GameBar
+
+# Bcr
+rm -rf vendor/bcr
+git clone https://github.com/kenway214/vendor_bcr.git vendor/bcr
+
+# Viper4A
+rm -rf packages/apps/ViPER4Android
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4Android
