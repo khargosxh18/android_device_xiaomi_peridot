43b57a2cd934e4a5a115fac09972da9b41475636
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-06-18 15:02:42 +0000

    Revert "peridot: Initial Aicp 21.2 Bringup!"
    This reverts commit 366f455846559e750acf1916784354ae88771f39.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aicp_peridot.mk
-
-COMMON_LUNCH_CHOICES := \
-    aicp_peridot-bp4a-userdebug \
-    aicp_peridot-bp4a-user \
-    aicp_peridot-bp4a-eng
+    $(LOCAL_DIR)/lineage_peridot.mk
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,7 @@ TARGET_KERNEL_EXT_MODULES := \
     nxp/opensource/driver
 
 # Partitions
--include vendor/aicp/config/BoardConfigReservedSize.mk
+-include vendor/lineage/config/BoardConfigReservedSize.mk
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
