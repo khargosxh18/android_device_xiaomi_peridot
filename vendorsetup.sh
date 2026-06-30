c9e36111be626d64f4a2270c78f5c5815159178a
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-06-30 04:16:35 +0000

    peridot: Switch to Sakura Keys

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Lineage Priv
-if [ ! -d "vendor/lineage-priv" ]; then
-    git clone https://github.com/khargosxh18/vendor_lineage-priv.git vendor/lineage-priv
+# Sakura Priv
+if [ ! -d "vendor/sakura-priv" ]; then
+    git clone https://github.com/Sakura-Devices/vendor_sakura-priv vendor/sakura-priv
 else
-    echo "vendor/lineage-priv already exists, skipping..."
+    echo "vendor/sakura-priv already exists, skipping..."
 fi
 
 # Gamebar
