6d020cadc64e3f6c28e3c6f3caa1f79aa2a8792e
Tim Zimmermann <tim@linux4.de>
2026-08-24 14:09:59 +0000

    peridot: Switch to pixel power-libperfmgr
    Change-Id: I2d2b99e050dfe23734439137f9fb5ed39808df71
    
    Signed-off-by: Khargosxh18 <nikhilyadav31071@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -238,7 +238,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
 include device/lineage/sepolicy/libion/sepolicy.mk
-include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/xiaomi/peridot/sepolicy/SEPolicy-diag.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
--- device.mk
+++ device.mk
@@ -412,7 +412,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
@@ -439,7 +439,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/google/pixel/pixelstats \
     hardware/google/pixel/power-libperfmgr \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
